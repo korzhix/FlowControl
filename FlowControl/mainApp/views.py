247ef044bcf184926da5d4cd86e8f4a61af4ea65
korzhof <korzhstud@gmail.com>
2020-03-02 23:11:17 +0300

    Disk page added, urls was bounded to links

--- FlowControl/mainApp/views.py
+++ FlowControl/mainApp/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, "mainApp/disk.html")
+
+def login_page(request):
+	return render(request, 'mainApp/login.html')
+
+def disk_page(request):
+	return render(request, 'mainApp/disk.html')
