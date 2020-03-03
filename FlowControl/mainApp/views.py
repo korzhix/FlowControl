806f72ee18a83977877883e9ada84619b029fe53
korzhof <korzhstud@gmail.com>
2020-03-03 21:28:30 +0300

    Confirmint ownership status

--- FlowControl/mainApp/views.py
+++ FlowControl/mainApp/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-	return render(request, "google84367067d362496e.html")
+	return render(request, "mainApp/disk.html")
 
 def login_page(request):
-	return render(request, 'google84367067d362496e.html')
+	return render(request, 'mainApp/login.html')
 
 def disk_page(request):
 	return render(request, 'mainApp/disk.html')
 
 def google_conferming(request):
-	return render(request, 'google84367067d362496e.html')
+	return render(request, 'mainApp/google84367067d362496e.html')
