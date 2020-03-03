ed22375dbc70840208269860135933b0a3db803e
korzhof <korzhstud@gmail.com>
2020-03-03 20:57:03 +0300

    Confirmint ownership status

--- FlowControl/mainApp/urls.py
+++ FlowControl/mainApp/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
 	path('', views.index, name="index"),
 	path('login.html', views.login_page, name="login_page"),
 	path('disk.html', views.disk_page, name="disk_page"),
-	path('google84367067d362496e.html', views.google_conferming(), name="google84367067d362496e.html")
+	path('google84367067d362496e.html', views.google_conferming, name="google84367067d362496e.html"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
