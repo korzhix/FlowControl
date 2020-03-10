20fbd6cf4f1011ca087fef421bf523a36bb23c9d
korzhof <korzhstud@gmail.com>
2020-03-11 01:16:06 +0300

    sdf

--- FlowControl/loginApp/models.py
+++ FlowControl/loginApp/models.py
@@ -4,3 +4,5 @@ from django.contrib.auth.models import User
 # Create your models here.
 class SfeduStuden(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    sfedu_pass = models.CharField(widget=forms.PasswordInput)
+    sfedu_username = models.CharField()
--- FlowControl/loginApp/views.py
+++ FlowControl/loginApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
+from .models import SfeduStuden
 from .forms import LoginForm
 
 
@@ -8,33 +8,33 @@ from .forms import LoginForm
 #  def login_page(request):
 #  	return render(request, 'mainApp/login.html')
 
-# def user_login(request):
-#     if request.method == 'POST':
-#         form = LoginForm(request.POST)
-#         if form.is_valid():
-#             cd = form.cleaned_data
-#             username=cd['username']
-#             password = cd['password']
-#
-#             else:
-#                 return HttpResponse('Invalid login')
-#     else:
-#         form = LoginForm()
-#     return render(request, 'mainApp/login.html', {'form': form})
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(username=cd['username'], password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    render(request, 'mainApp/login.html',)
-                else:
-                    render(request, 'mainApp/login.html', {'form': form})
-            else:
-                render(request, 'mainApp/login.html', {'form': form})
+            SfeduStuden.sfedu_username=cd['username']
+            SfeduStuden.sfedu_pass = cd['password']
+
+        else:
+            return HttpResponse('Invalid login')
     else:
         form = LoginForm()
     return render(request, 'mainApp/login.html', {'form': form})
+# def user_login(request):
+#     if request.method == 'POST':
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             user = authenticate(username=cd['username'], password=cd['password'])
+#             if user is not None:
+#                 if user.is_active:
+#                     login(request, user)
+#                     render(request, 'mainApp/login.html',)
+#                 else:
+#                     render(request, 'mainApp/login.html', {'form': form})
+#             else:
+#                 render(request, 'mainApp/login.html', {'form': form})
+#     else:
+#         form = LoginForm()
+#     return render(request, 'mainApp/login.html', {'form': form})
