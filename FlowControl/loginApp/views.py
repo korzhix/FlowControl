06401e55da2b16bfff8893a7fdd30cfff741f929
korzhof <korzhstud@gmail.com>
2020-03-11 00:50:04 +0300

    sdf

--- FlowControl/loginApp/views.py
+++ FlowControl/loginApp/views.py
@@ -1,5 +1,40 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
+
 
 # Create your views here.
-def login_page(request):
-	return render(request, 'mainApp/login.html')
+#  def login_page(request):
+#  	return render(request, 'mainApp/login.html')
+
+# def user_login(request):
+#     if request.method == 'POST':
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             username=cd['username']
+#             password = cd['password']
+#
+#             else:
+#                 return HttpResponse('Invalid login')
+#     else:
+#         form = LoginForm()
+#     return render(request, 'mainApp/login.html', {'form': form})
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    render(request, 'mainApp/login.html',)
+                else:
+                    render(request, 'mainApp/login.html', {'form': form})
+            else:
+                render(request, 'mainApp/login.html', {'form': form})
+    else:
+        form = LoginForm()
+    return render(request, 'mainApp/login.html', {'form': form})
