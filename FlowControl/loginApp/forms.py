12731524721e9ecad96e1fea1b60f6519a323262
korzhof <korzhstud@gmail.com>
2020-03-11 23:07:52 +0300

    sdf

--- FlowControl/loginApp/forms.py
+++ FlowControl/loginApp/forms.py
@@ -1,5 +1,10 @@
 from django import forms
+from 
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
--- FlowControl/loginApp/views.py
+++ FlowControl/loginApp/views.py
@@ -7,7 +7,7 @@ from .forms import LoginForm
 # Create your views here.
 #  def login_page(request):
 #  	return render(request, 'mainApp/login.html')
-
+#
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
@@ -20,21 +20,21 @@ def user_login(request):
             return HttpResponse('Invalid login')
     else:
         form = LoginForm()
-    return render(request, 'mainApp/login.html', {'form': form})
-# def user_login(request):
-#     if request.method == 'POST':
-#         form = LoginForm(request.POST)
-#         if form.is_valid():
-#             cd = form.cleaned_data
-#             user = authenticate(username=cd['username'], password=cd['password'])
-#             if user is not None:
-#                 if user.is_active:
-#                     login(request, user)
-#                     render(request, 'mainApp/login.html',)
-#                 else:
-#                     render(request, 'mainApp/login.html', {'form': form})
-#             else:
-#                 render(request, 'mainApp/login.html', {'form': form})
-#     else:
-#         form = LoginForm()
-#     return render(request, 'mainApp/login.html', {'form': form})
+    return render(request, 'loginApp/login.html', {'form': form})
+# # def user_login(request):
+# #     if request.method == 'POST':
+# #         form = LoginForm(request.POST)
+# #         if form.is_valid():
+# #             cd = form.cleaned_data
+# #             user = authenticate(username=cd['username'], password=cd['password'])
+# #             if user is not None:
+# #                 if user.is_active:
+# #                     login(request, user)
+# #                     render(request, 'mainApp/login.html',)
+# #                 else:
+# #                     render(request, 'mainApp/login.html', {'form': form})
+# #             else:
+# #                 render(request, 'mainApp/login.html', {'form': form})
+# #     else:
+# #         form = LoginForm()
+# #     return render(request, 'mainApp/login.html', {'form': form})
