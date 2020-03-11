41aff7fa1418c363e600e2d1f96aa79c3f1f7a51
korzhof <korzhstud@gmail.com>
2020-03-11 23:07:52 +0300

    sdf

--- FlowControl/FlowControl/urls.py
+++ FlowControl/FlowControl/urls.py
@@ -14,14 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.urls import include
 from django.contrib.auth import views as auth_views
 from mainApp import  views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mainApp.urls')),
     path('social-auth/', include('social_django.urls', namespace="social")),
-    path("login/", include(('loginApp.urls', 'loginApp'), namespace="login"))
+    re_path(r"login*", include('loginApp.urls')),
+    path('', include('mainApp.urls')),
 ]
--- FlowControl/loginApp/urls.py
+++ FlowControl/loginApp/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
+from django.urls import path, re_path
 """FlowControl URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -16,13 +17,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from django.urls import re_path
-from django.urls import include
 from . import views
 
 urlpatterns = [
-	#path('login.html', views.login_page, name="login_page"),
-    url('login.html', views.user_login, name='login'),
-    url('', views.user_login)
+    re_path(r'.*', views.user_login, name='login'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
