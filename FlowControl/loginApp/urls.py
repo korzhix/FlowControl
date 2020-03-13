f0dbc329ca9c75ab0b3505e249fafa849eda1755
korzhof <korzhstud@gmail.com>
2020-03-13 18:22:09 +0300

    sd

--- FlowControl/loginApp/urls.py
+++ FlowControl/loginApp/urls.py
@@ -1,24 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url
 from django.urls import path, re_path
-"""FlowControl URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from . import views
 
+
 urlpatterns = [
+    re_path('login/logout.html', views.LogoutView.as_view()),
+    re_path("form.html", views.LoginFormView.as_view()),
+    re_path(r'register.html', views.RegisterFormView.as_view()),
     re_path(r'.*', views.user_login, name='login'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
