7282e5654b18b1719567fb2f06c754e3ee2906c9
korzhof <korzhstud@gmail.com>
2020-03-29 21:59:00 +0300

    trying to generate sidebar

--- FlowControl/mainApp/views.py
+++ FlowControl/mainApp/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 
+from accountApp.models import Profile
 # Create your views here.
 
 def index(request):
-	return render(request, "mainApp/disk.html")
+	if request.user.pk is not None:
+		student = student = Profile.objects.get(pk=request.user.pk)
+		return render(request, "mainApp/disk.html", {'student': student})
+	return render(request, 'mainApp/disk.html')
 
 def disk_page(request):
+	if request.user.pk is not None:
+		student = Profile.objects.get(pk=request.user.pk)
+		return render(request, "mainApp/disk.html", {'student': student})
+
 	return render(request, 'mainApp/disk.html')
