eb17419eee6da3da591b21db0a6230b47ac38dad
BarmaJley <vadimchaykin2015@yandex.ru>
2020-03-31 17:17:45 +0300

    add

--- FlowControl/accountApp/static/accountApp/js/user_script.js
+++ FlowControl/accountApp/static/accountApp/js/user_script.js
@@ -1,3 +1,19 @@
 const pass1 = document.getElementById('id_password1');
+const pass = document.getElementById('id_password');
+const pass2 = document.getElementById('id_password2');
+const user = document.getElementById('id_username');
+if (pass1 !== null){
+	pass1.placeholder = "EmailID";
+}
+if (pass !== null){
+	pass.placeholder = "Password";
+}
+if (pass2 !== null){
+	pass2.placeholder = "Password";
+}
+if (user !== null){
+	user.placeholder = "Username";
+}
 
-console.log(pass1)
+
+
